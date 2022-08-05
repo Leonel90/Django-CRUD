1126a15c27af9678c063291d3d6b0a55b9fe2c28
Leonel Sangolquiza <97122219+Leonel90@users.noreply.github.com>
2022-08-04 23:14:37 -0500

    añadimos boton eliminar y su funcionalidad

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,9 @@
 from venv import create
 from django.urls import path
-from .views import create_task, list_tasks
+from .views import create_task, list_tasks, delete_task
 
 urlpatterns = [
     path('', list_tasks),
-    path('new/', create_task, name='create_task')
+    path('new/', create_task, name='create_task'),
+    path('delete_task/<int:task_id>/', delete_task, name='delete_task')
 ]
