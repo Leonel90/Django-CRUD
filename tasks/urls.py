7fc54f937e040728efd2c9c12159a800899d76b9
Leonel Sangolquiza <97122219+Leonel90@users.noreply.github.com>
2022-08-04 22:15:38 -0500

    Crud guardar

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,8 @@
+from venv import create
 from django.urls import path
-from .views import list_tasks
+from .views import create_task, list_tasks
 
 urlpatterns = [
-    path('', list_tasks)
+    path('', list_tasks),
+    path('new/', create_task)
 ]
